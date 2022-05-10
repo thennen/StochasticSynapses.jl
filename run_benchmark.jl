5e5ded00cee402168ee179fd41ce64247227ffc3
Tyler <tyler@hennen.us>
2022-05-10 03:04:24 -0700

    Remove unneeded benchmark files

--- run_benchmark.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-Pkg.activate("StochasticSynapses")
-using StochasticSynapses
-include(".\\benchmark\\benchmarks.jl")
-#standard_benchmark_threaded("threads_order_benchmark")
-#benchmark_read_write_threaded("threaded_IO_benchmark")
-run_arrayofstruct_benchmarks()
